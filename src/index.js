f0a81ea26ef37a5e15b38cd085ec81731ab7ca40
jorge <eduardcrow555@gmail.com>
2022-06-01 18:22:35 -0300

    final

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Home from 'views/Home';
+import './assets/styles/main.css'; 
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    {/* invoca o componente entre tags */}
+    <Home/>
+  </React.StrictMode>
+);
