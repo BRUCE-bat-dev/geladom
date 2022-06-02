b230afcf6349432feff704e9c58daa91a855166d
jorge <eduardcrow555@gmail.com>
2022-06-02 13:47:01 -0300

    api

--- src/helpers/Api.js
+++ src/helpers/Api.js
@@ -6,7 +6,6 @@ const PaletaContext = {
     updatePaletaById: (id) => `${PaletaContext.paletaEndpoint()}/update-paleta/${id}`,
     deletePaletaById: (id) => `${PaletaContext.paletaEndpoint()}/delete-paleta/${id}`,
 };
-
 export const Api = {
     baseUrl: "https://api-elgeladon.herokuapp.com",
     ...PaletaContext,
